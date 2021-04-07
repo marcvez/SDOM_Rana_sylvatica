df235ae6d8ce8c3b0bb0223123eb51c4563c8f1a
Marc Vez <marc.99.om@gmail.com>
2021-04-07 17:50:41 +0200

    Irja's commit

--- backwards_model_Irja.R
+++ backwards_model_Irja.R
@@ -12,7 +12,6 @@
 ##needed libraries
 library(plot.matrix)
 
-
 Decisions <- function (prob_good_temp, time_steps) {
   
   # Parameters
@@ -155,6 +154,7 @@ par(mar=c(5.1, 4.5, 4.1, 6.5))
 # Plot
 
 Decisions(prob_good_temp, prob_bad_temp, effect_good_temp, effect_bad_temp, time_steps)
+
 
 
 
