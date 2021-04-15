7b8b7db3e6a5db87411f91adab2764889d1b2878
Marc Vez <marc.99.om@gmail.com>
2021-04-15 15:40:06 +0200

    I've upgraded the storing system, I've write the code for the maximum trait values (it was a problem) and I've ploted some things

--- Forward_model.R
+++ Forward_model.R
@@ -12,27 +12,42 @@
 N <- 100
 # Number of Tadpoles
 
-Population <- matrix(nrow = N, ncol = time_steps + 1)
+Population <- matrix(nrow = N, ncol = time_steps + 3)
 rownames(Population) <- c(1:N) # Tadpole number xx
-colnames(Population) <- c((1:time_steps), "Fitness") # Time step xx
+colnames(Population) <- c((1:time_steps), "Size", "Performance", "Fitness") # Time step xx
 # This matrix is going to store the state of each tadpole during the whole
-# period. It is going to be read by the alive matrix, to determine how many 
-# individuals are still alive at each time step. 
+# period. It is going to be read by the "Alive" matrix, to determine how many 
+# individuals are still alive at each time step. The last columns store the Size,
+# Performance and fitness that tadpoles have at the last time step.
 
 
-Alive <- matrix(nrow = time_steps, ncol = 2)
-colnames(Alive) <- c("time step", "Nº alive")
-Alive[, 1] <- c(1:time_steps)
+Alive <- as.data.frame(matrix(nrow = time_steps + 1, ncol = 2))
+colnames(Alive) <- c("time_step", "N_alive")
+Alive[, 1] <- c(0:time_steps)
+Alive[1, 2] <- N
 # We are going to sum the number of dead or alive individuals at each time step.
 # For ploting.
 
+Tadpole <- c(1:N)
+State <- c("Size", "Performance", "Fitness")
+Time <- c(0:time_steps)
+Tadpole_state <- array(NA, dim = c(N, 3, time_steps + 1), dimnames=list (Tadpole, State, Time))
+Tadpole_state[, 1, 1] <- Size[2]
+Tadpole_state[, 2, 1] <- Performance[1]
+Tadpole_state[, 3, 1] <- Fitness[1,1,1]
+# This array stores the Size, Performance and Expected Fitness values of each tadpole at each time step.
+
+Performance_forw <- Performance
 
 for (n in 1:N) {
   
   i <- 2
   j <- 1
   t <- 1
-  # Initial condition, after hatching
+  Size[i] <- Size[2]
+  Performance_forw[j] <- Performance[1]
+  
+  # Initial condition, after hatching (This could be a little bit stochastic)
   
   while (t <= time_steps) {
     
@@ -40,39 +55,102 @@ for (n in 1:N) {
     # Random value that determines if you live or you die.
     
     if (Prob_Survive < Survival[i, j]) {
-      # Prob_Survive where 0.7
-      if (ForageRule[i, j, t] == TRUE) {
-        
-        j <- j + 1
+      # If you survive...
+      
+      # This is how it's going to Work: If at this time step and combination of Size and Performance, on your ForageRule array it says that you should invest in performance (TRUE), you add 1 to this state (j). If it says that you should invest in growth (FALSE), you add one to the growth state (i).
+      # Then you write 1 on the Population matrix, in the correct time step and tadpole ID.
+  
+      if (ForageRule[i, j, t] == TRUE & i == max_Size & j == max_Performance) {
+    
+        j <- j
         
         Population[n, t] <- 1
         
-        print("Performance")
+      } else if (ForageRule[i, j, t] == FALSE & i == max_Size & j == max_Performance) {
         
-      } else if (ForageRule[i, j, t] == FALSE) {
+        i <- i
+        
+        Population[n, t] <- 1
+        
+      } # MAx Performance and MAX Size -> You stay the same
+      
+      
+      else if (ForageRule[i, j, t] == TRUE & i < max_Size & j == max_Performance) {
+        
+        j <- j
+        
+        Population[n, t] <- 1
+        
+      } else if (ForageRule[i, j, t] == FALSE & i < max_Size & j == max_Performance) {
         
         i <- i + 1
         
         Population[n, t] <- 1
         
-        print("Growth")
+      } # MAX Performance but Size < Max -> You can grow, but not increase your Performance
+      
+      
+      else if (ForageRule[i, j, t] == TRUE & i == max_Size & j < max_Performance) {
         
-      }
+        j <- j + 1
+        
+        Population[n, t] <- 1
+        
+      } else if (ForageRule[i, j, t] == FALSE & i == max_Size & j < max_Performance) {
+        
+        i <- i
+        
+        Population[n, t] <- 1
+        
+      } # MAX Size but Performance < Max -> You can't grow, but you can improve Performance
+      
+      
+      else if (ForageRule[i, j, t] == TRUE & i < max_Size & j < max_Performance) {
+        
+        j <- j + 1
+        
+        Population[n, t] <- 1
+        
+      } else if (ForageRule[i, j, t] == FALSE & i < max_Size & j < max_Performance) {
+       
+        i <- i + 1
+       
+        Population[n, t] <- 1
+       
+      } # Normal situation, you can always Grow or improve Performance
+      
+      
       
     } else {
+      # On the contrary, if you are dead,
       
       i <- 1
+      j <- 1
+      Performance_forw[j] <- 0
+      # Your state is 1 for Survival and everything, and you are going to stay
+      # this way forever.
       
       Population[n, t] <- 0
-      
-      print("Dead")
+      # A 0 in the Population matrix symbolizes that you are dead
+     
+    } # if/else loop (dead/alive)
     
-      
-    } # if/else loop
+    Alive[t + 1, 2] <- sum(Population[, t])
+    # Store the number of tadpoles that are alive at each time step.
+    # There is a time step 0, and it's the initial population.
     
-    Alive[t, 2] <- sum(Population[, t])
     
-    Population[n, time_steps + 1] <- Fitness[i, j, t]
+    Tadpole_state[n, 1, t + 1] <- Size[i]
+    Tadpole_state[n, 2, t + 1] <- Performance_forw[j]
+    Tadpole_state[n, 3, t + 1] <- Fitness[i, j, t]
+    # We store the Size and Performance of each tadpole at each time Step
+    
+    
+    Population[n, time_steps + 1] <- Size[i]
+    Population[n, time_steps + 2] <- Performance_forw[j]
+    Population[n, time_steps + 3] <- Fitness[i, j, t]
+    # We write the final Size, Performance and Fitness of each Tadpole at the end 
+    # of the Population matrix.
     
     t <- t + 1 
     
@@ -83,6 +161,33 @@ for (n in 1:N) {
 
 print(Alive[time_steps, 2])
 
+par(mfrow=c(1,1))
 
+ggplot(data = Alive, aes(x = time_step, y = N_alive, group=1)) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(name="Time Steps", limits=c(0, time_steps)) +
+  scale_y_continuous(name="Nº tadpoles alive", limits=c(0, N))
+
+
+
+
+par(mfrow=c(2,1))
+par(mar=c(5.1, 4.5, 1, 6.5))
+
+plot(1, type="l", xlab="Time Step", ylab="Size", xlim=c(1, time_steps), ylim=c(0, Size[max_Size]), xaxt = "n")
+axis(1, at=1:(time_steps + 1), labels = c(0:time_steps))
+for (n in 1:N) {
+  lines(Tadpole_state[n, 1, ])
+  
+}
+  
+plot(1, type="l", xlab="Time Step", ylab="Performance", xlim=c(1, time_steps), ylim=c(0, Performance[max_Performance]), xaxt = "n")
+axis(1, at=1:(time_steps + 1), labels = c(0:time_steps))
+
+for (n in 1:N) {
+  lines(Tadpole_state[n, 2, ])
+  
+}
 
 
