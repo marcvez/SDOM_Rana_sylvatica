ec9580509d89b7ad8eefd8ecb730296849f32a7d
Marc Vez <marc.99.om@gmail.com>
2021-05-23 22:46:32 +0200

    Some adjustments. Final version?

--- new_Forward_model.R
+++ new_Forward_model.R
@@ -40,7 +40,7 @@ Forward <- function(N) {
   for (n in 1:N) {
     
     i <- 2 # sample(2:4,1)
-    j <- 1 + prob_bad_temp * 10 # sample(1:3,1)
+    j <- 1 # + prob_bad_temp * 30 # sample(1:3,1)
     k <- 1
     t <- 1
     
@@ -386,7 +386,7 @@ Comparison_plot <- function(){
     prob_good_temp
     prob_bad_temp <- 1 - prob_good_temp
     days <- 60
-    end_season_percentage <- 0.4  
+    end_season_percentage <- 0.2
     end_season_intensity <- 1 
     death_rate_day <- 0.012 
     N <- 100
@@ -403,7 +403,7 @@ Comparison_plot <- function(){
     plot(density(Final_results[,2], bw = 0.1, from = -0.5, to = max(Performance) + 0.4), col = "black", main = paste("Final Traits (blue = F, red = S, black = P) prob good temp = ", prob_good_temp))
     abline(v = mean(Performance_bigger_0))
     
-    lines(density(Final_results[,1], bw = 0.1, from = 1, to = max(Size) + 0.1), col = "red")
+    lines(density(Final_results[,1], bw = 0.1, from = 1, to = max(Size) + 0.5), col = "red")
     abline(v = mean(Size_bigger_0), col = "red")
     
     lines(density(Final_results[,3], bw = 0.1, from = 1, to = max(Fitness[, , 10, time_steps + 1]) + 0.5), col = "blue")
@@ -418,6 +418,46 @@ Comparison_plot <- function(){
   
 }
 
+Comparison_plot_little <- function(){
+  
+  par(mfrow=c(3,1))
+  
+  for(prob_good_temp in c(0.3, 0.5, 0.7)){
+    
+    prob_good_temp
+    prob_bad_temp <- 1 - prob_good_temp
+    days <- 60
+    end_season_percentage <- 0.2  
+    end_season_intensity <- 1 
+    death_rate_day <- 0.012 
+    N <- 100
+    
+    
+    Decisions(prob_good_temp, prob_bad_temp, days, end_season_percentage, end_season_intensity, death_rate_day)
+    
+    Forward(N)
+    
+    Fitness_bigger_0 <- as.vector(Final_Fitness[Final_Fitness > 0])
+    Performance_bigger_0 <- as.vector(Final_Performance[Final_Performance > 0])
+    Size_bigger_0 <- as.vector(Final_Size[Final_Size > 0])
+    
+    plot(density(Final_results[,2], bw = 0.1, from = -0.5, to = max(Performance) + 0.4), col = "black", main = paste("Final Traits (blue = F, red = S, black = P) prob good temp = ", prob_good_temp))
+    abline(v = mean(Performance_bigger_0))
+    
+    lines(density(Final_results[,1], bw = 0.1, from = 1, to = max(Size) + 0.5), col = "red")
+    abline(v = mean(Size_bigger_0), col = "red")
+    
+    lines(density(Final_results[,3], bw = 0.1, from = 1, to = max(Fitness[, , 10, time_steps + 1]) + 0.5), col = "blue")
+    abline(v = mean(Fitness_bigger_0), col = "blue")
+    
+    abline(h = 0)
+    minor.tick(nx=10, ny=1)
+    
+    rm(list = ls())
+    
+  }
+  
+}
 
 
 # Initial parameters
@@ -446,6 +486,7 @@ Histogram_plot()
 
 Comparison_plot()
 
+Comparison_plot_little()
 
 
 
